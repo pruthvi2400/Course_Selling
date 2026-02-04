341c7456a1fe6fcf9dc3351a6f1aca97c21e0f18
Pruthviraj Sonawane <pruthvirajsonawane74@gmail.com>
2026-02-05 00:24:03 +0530

    Fix reactive form initialization

--- database.js
+++ database.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://Pruthvi:H7wW3T85KoZvTIwP@cluster0.avlsfod.mongodb.net/Pruthvis-course1");
-const Schema=mongoose.Schema;
-const ObjectId=mongoose.ObjectId;
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
 
-const user=new Schema({
+const user = new Schema({
 
         email:{type: String,unique: true},
         password:String,
         firstName:String,
         lastName:String
 
-})
+});
 
-const admin=new Schema({
+const admin = new Schema({
 
         email:{type: String,unique: true},
         password:String,
@@ -20,9 +19,9 @@ const admin=new Schema({
         lastName:String
 
        
-})
+});
 
-const course=new Schema({
+const course = new Schema({
 
         
         title:String,
@@ -30,19 +29,19 @@ const course=new Schema({
         price:Number,
         imageURL:String,
         creatorID:ObjectId,
-})
+});
 
-const purches=new Schema({
+const purches = new Schema({
 
         courseId:ObjectId,
         userId:ObjectId,
 
-})
+});
 
-const UserModel=mongoose.model("user",user)
-const adminModel=mongoose.model("admin",admin)
-const courseModel=mongoose.model("course",course)
-const purchesModel=mongoose.model("purchaes",purches)
+const UserModel = mongoose.model("user", user);
+const adminModel = mongoose.model("admin", admin);
+const courseModel = mongoose.model("course", course);
+const purchesModel = mongoose.model("purchaes", purches);
 
 module.exports={
     UserModel:UserModel,
@@ -50,4 +49,4 @@ module.exports={
     courseModel:courseModel,
      purchesModel:purchesModel
 
-}
+};
